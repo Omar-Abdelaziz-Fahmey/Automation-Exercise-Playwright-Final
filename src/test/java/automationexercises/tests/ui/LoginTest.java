083ee0b57b6611dea6147ee2714756504d64fb0d
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-11-29 20:16:05 +0200

    Add LoginTest class to implement user login functionality tests

---
+++ src/test/java/automationexercises/tests/ui/LoginTest.java
@@ -0,0 +1,125 @@
+package automationexercises.tests.ui;
+
+import automationexercises.apis.UserManagementAPI;
+import automationexercises.base.BaseTest;
+import automationexercises.pages.SignupLoginPage;
+import automationexercises.utils.TimeManager;
+import automationexercises.utils.dataReader.JsonReader;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Test;
+
+
+@Epic("Automation Exercise")
+@Feature("UI User Management")
+@Story("User Login")
+@Severity(SeverityLevel.CRITICAL)
+@Owner("Omar")
+public class LoginTest extends BaseTest {
+    String timestamp = TimeManager.getSimpleTimestamp();
+    JsonReader testData = new JsonReader("login-data");
+    //Tests
+
+    @Description("Verify user can login with valid credentials")
+    @Test
+    public void loginWithValidCredentialsTest() {
+        // Test logic for logging in with valid credentials
+        new UserManagementAPI().createRegisterUserAccount(
+                testData.getJsonData("name"),
+                testData.getJsonData("email") + timestamp + "@gmail.com",
+                testData.getJsonData("password"),
+                testData.getJsonData("firstName"),
+                testData.getJsonData("lastName")
+        ).verifyUserCreatedSuccessfully();
+
+        new SignupLoginPage(page).navigate()
+                .enterLoginEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
+                .enterLoginPassword(testData.getJsonData("password"))
+                .clickLoginButton()
+                .navigationBar
+                .verifyLoggedInUserName(testData.getJsonData("name"));
+        new UserManagementAPI().deleteUserAccount(
+                        testData.getJsonData("email") + timestamp + "@gmail.com",
+                        testData.getJsonData("password"))
+                .verifyUserDeletedSuccessfully();
+    }
+
+    @Description("Verify user can't login with invalid email")
+    @Test
+    public void loginWithInvalidEmailTest() {
+        // Test logic for logging in with an invalid email
+        new UserManagementAPI().createRegisterUserAccount(
+                testData.getJsonData("name"),
+                testData.getJsonData("email") + timestamp + "@gmail.com",
+                testData.getJsonData("password"),
+                testData.getJsonData("firstName"),
+                testData.getJsonData("lastName")
+        ).verifyUserCreatedSuccessfully();
+
+        new SignupLoginPage(page).navigate()
+                .enterLoginEmail(testData.getJsonData("email") + timestamp +timestamp+ "@gmail.com")
+                .enterLoginPassword(testData.getJsonData("password"))
+                .clickLoginButton()
+                .verifyLoginErrorMessage(testData.getJsonData("messages.error"));
+        new UserManagementAPI().deleteUserAccount(
+                        testData.getJsonData("email") + timestamp + "@gmail.com",
+                        testData.getJsonData("password"))
+                .verifyUserDeletedSuccessfully();
+
+    }
+
+
+    @Description("Verify user can't login with incorrect password")
+    @Test
+    public void loginWithIncorrectPasswordTest() {
+        // Test logic for logging in with an incorrect password
+        new UserManagementAPI().createRegisterUserAccount(
+                testData.getJsonData("name"),
+                testData.getJsonData("email") + timestamp + "@gmail.com",
+                testData.getJsonData("password"),
+                testData.getJsonData("firstName"),
+                testData.getJsonData("lastName")
+        ).verifyUserCreatedSuccessfully();
+
+        new SignupLoginPage(page).navigate()
+                .enterLoginEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
+                .enterLoginPassword(testData.getJsonData("password")+timestamp)
+                .clickLoginButton()
+                .verifyLoginErrorMessage(testData.getJsonData("messages.error"));
+
+        new UserManagementAPI().deleteUserAccount(
+                        testData.getJsonData("email") + timestamp + "@gmail.com",
+                        testData.getJsonData("password"))
+                .verifyUserDeletedSuccessfully();
+    }
+    @Description("Verify user can logout")
+    @Test
+    public void logoutTest(){
+        new UserManagementAPI().createRegisterUserAccount(
+                testData.getJsonData("name"),
+                testData.getJsonData("email") + timestamp + "@gmail.com",
+                testData.getJsonData("password"),
+                testData.getJsonData("firstName"),
+                testData.getJsonData("lastName")
+        ).verifyUserCreatedSuccessfully();
+
+        new SignupLoginPage(page).navigate()
+                .enterLoginEmail(testData.getJsonData("email") + timestamp + "@gmail.com")
+                .enterLoginPassword(testData.getJsonData("password"))
+                .clickLoginButton()
+                .navigationBar
+                .verifyLoggedInUserName(testData.getJsonData("name"))
+                .clickOnLogoutButton()
+                .navigationBar
+                .verifyLogoutButtonNotVisible()
+                .verifySignupLabelVisible();
+
+        new UserManagementAPI().deleteUserAccount(
+                        testData.getJsonData("email") + timestamp + "@gmail.com",
+                        testData.getJsonData("password"))
+                .verifyUserDeletedSuccessfully();
+
+    }
+
+
+
+}

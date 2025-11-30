e40b8d2d60ecd2edfea9d13de45ccfdc30a628be
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-11-30 12:43:05 +0200

    Add ContactUsTest class to validate navigation and form submission for Contact Us page

---
+++ src/test/java/automationexercises/tests/ui/ContactUsTest.java
@@ -0,0 +1,78 @@
+package automationexercises.tests.ui;
+
+import automationexercises.base.BaseTest;
+import automationexercises.pages.ContactUsPage;
+import automationexercises.pages.components.NavigationBarComponent;
+import automationexercises.utils.dataReader.JsonReader;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.Test;
+
+@Epic("Automation Exercise")
+@Feature("UI Contact Us Management")
+@Owner("Omar")
+public class ContactUsTest extends BaseTest {
+        JsonReader testData = new JsonReader("contactus-data");
+
+        @Test
+        @Story("Contact Us Page Navigation - Happy Path")
+        @Description("Verify that users can navigate to the Contact Us page from the navigation bar")
+        @Severity(SeverityLevel.NORMAL)
+        public void navigateToContactUsPageTest() {
+                new NavigationBarComponent(page)
+                                .clickOnContactUsButton()
+                                .verifyContactUsPageIsDisplayed();
+        }
+
+        @Test
+        @Story("Submit Contact Us Form without Attachment - Happy Path")
+        @Description("Verify that users can successfully submit the Contact Us form with valid data without attachment")
+        @Severity(SeverityLevel.CRITICAL)
+        public void submitContactUsFormWithValidDataWithoutAttachmentTest() {
+                // Test implementation goes
+                new ContactUsPage(page).navigate()
+                                .fillContactUsForm(
+                                                testData.getJsonData("contacts.name"),
+                                                testData.getJsonData("contacts.email"),
+                                                testData.getJsonData("contacts.subject"),
+                                                testData.getJsonData("contacts.message"))
+                                .verifyAlertMessageAfterSubmit(testData.getJsonData("messages.alertMessage"))
+                                .clickSubmitButton()
+                                .verifySuccessMessageAfterSubmit(testData.getJsonData("messages.successMessage"));
+
+        }
+
+        @Test
+        @Story("Submit Contact Us Form with Attachment - Happy Path")
+        @Description("Verify that users can successfully submit the Contact Us form with valid data and an attachment")
+        @Severity(SeverityLevel.CRITICAL)
+        public void submitContactUsFormWithAttachmentTest() {
+                // Test implementation goes here
+                new ContactUsPage(page).navigate()
+                                .fillContactUsForm(
+                                                testData.getJsonData("contacts.name"),
+                                                testData.getJsonData("contacts.email"),
+                                                testData.getJsonData("contacts.subject"),
+                                                testData.getJsonData("contacts.message"),
+                                                testData.getJsonData("contacts.filePath"))
+                                .verifyAlertMessageAfterSubmit(testData.getJsonData("messages.alertMessage"))
+                                .clickSubmitButton()
+                                .verifySuccessMessageAfterSubmit(testData.getJsonData("messages.successMessage"));
+
+        }
+
+        @Test
+        @Story("Submit Contact Us Form with Empty Email - Negative")
+        @Description("Verify that users cannot submit the Contact Us form with an empty Email field")
+        @Severity(SeverityLevel.NORMAL)
+        public void submitContactUsFormWithEmptyEmailTest() {
+                new ContactUsPage(page).navigate()
+                                .fillContactUsForm(
+                                                testData.getJsonData("contacts.name"),
+                                                "",
+                                                testData.getJsonData("contacts.subject"),
+                                                testData.getJsonData("contacts.message"))
+                                .clickSubmitButton()
+                                .verifyEmailFieldValidationMessage();
+        }
+
+}

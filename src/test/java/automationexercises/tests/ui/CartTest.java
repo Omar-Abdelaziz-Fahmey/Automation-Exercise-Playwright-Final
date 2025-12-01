afeb4d77150b86609d6d287ffbaec998924ff788
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-12-01 14:05:23 +0200

    Switch from JUnit to TestNG annotations and add test groups for cart regression tests

--- src/test/java/automationexercises/tests/ui/CartTest.java
+++ src/test/java/automationexercises/tests/ui/CartTest.java
@@ -4,51 +4,49 @@ import automationexercises.base.BaseTest;
 import automationexercises.pages.ProductsPage;
 import automationexercises.utils.dataReader.JsonReader;
 import io.qameta.allure.*;
-import org.junit.jupiter.api.Test;
-
+import org.testng.annotations.Test;
 
 @Epic("Automation Exercise")
 @Feature("UI Product Management")
 @Story("Cart Management")
 @Owner("Omar")
 public class CartTest extends BaseTest {
-    JsonReader testData = new JsonReader("cart-data");
+        JsonReader testData = new JsonReader("cart-data");
 
+        @Test(groups = { "cart", "regression" })
+        @Story("Cart Management")
+        @Description("Verify product details on cart without login")
+        @Severity(SeverityLevel.CRITICAL)
 
-    @Test
-    @Severity(SeverityLevel.CRITICAL)
+        public void verifyProductDetailsOnCartWithOutLogInTC() {
+                new ProductsPage(page).navigationBar.clickProductsButton()
+                                .clickOnAddToCart(testData.getJsonData("product.name"))
+                                .validateProductAddedToCart(testData.getJsonData("messages.cartAdded"))
+                                .clickOnViewCart()
+                                .verifyProductDetailsOnCart(
+                                                testData.getJsonData("product.name"),
+                                                testData.getJsonData("product.price"),
+                                                testData.getJsonData("product.quantity"),
+                                                testData.getJsonData("product.total"));
+        }
 
-    public void verifyProductDetailsOnCartWithOutLogInTC(){
-        new ProductsPage(page).navigationBar.clickProductsButton()
-                .clickOnAddToCart(testData.getJsonData("product.name"))
-                .validateProductAddedToCart(testData.getJsonData("messages.cartAdded"))
-                .clickOnViewCart()
-                .verifyProductDetailsOnCart(
-                        testData.getJsonData("product.name"),
-                        testData.getJsonData("product.price"),
-                        testData.getJsonData("product.quantity"),
-                        testData.getJsonData("product.total")
-                );
-    }
-    @Test
-    @Description("Verify product quantity in cart")
-    @Severity(SeverityLevel.CRITICAL)
-    public void verifyProductQuantityInCartTC(){
-        new ProductsPage(page).navigationBar.clickProductsButton()
-                .clickOnViewProduct(testData.getJsonData("product2.name"))
-                .setProductQuantity(testData.getJsonData("product2.quantity"))
-                .clickOnAddToCart()
-                .validateProductAddedToCart(testData.getJsonData("messages.cartAdded"))
-                .clickOnViewCart()
-                .verifyProductDetailsOnCart(
-                        testData.getJsonData("product2.name"),
-                        testData.getJsonData("product2.price"),
-                        testData.getJsonData("product2.quantity"),
-                        testData.getJsonData("product2.total")
-                );
+        @Test(groups = { "cart", "regression" })
+        @Story("Cart Management")
+        @Description("Verify product quantity in cart")
+        @Severity(SeverityLevel.CRITICAL)
+        public void verifyProductQuantityInCartTC() {
+                new ProductsPage(page).navigationBar.clickProductsButton()
+                                .clickOnViewProduct(testData.getJsonData("product2.name"))
+                                .setProductQuantity(testData.getJsonData("product2.quantity"))
+                                .clickOnAddToCart()
+                                .validateProductAddedToCart(testData.getJsonData("messages.cartAdded"))
+                                .clickOnViewCart()
+                                .verifyProductDetailsOnCart(
+                                                testData.getJsonData("product2.name"),
+                                                testData.getJsonData("product2.price"),
+                                                testData.getJsonData("product2.quantity"),
+                                                testData.getJsonData("product2.total"));
 
-    }
-
-
+        }
 
 }

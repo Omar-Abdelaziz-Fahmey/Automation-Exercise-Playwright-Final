44a59984ee47850c055718d014feb40cebb6a3f7
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-12-01 14:06:59 +0200

    Refactor PaymentPage: replace JUnit assertions with TestNG, improve code formatting and readability

--- src/main/java/automationexercises/pages/PaymentPage.java
+++ src/main/java/automationexercises/pages/PaymentPage.java
@@ -1,16 +1,14 @@
 package automationexercises.pages;
 
-
 import automationexercises.FileUtils;
 import com.microsoft.playwright.Download;
 import com.microsoft.playwright.Page;
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.Assertions;
+import org.testng.Assert;
 
 import java.nio.file.Paths;
 
 public class PaymentPage {
-
 
     private final Page page;
 
@@ -18,11 +16,10 @@ public class PaymentPage {
         this.page = page;
     }
 
-
-    //vars
+    // vars
     private String paymentEndpoint = "/payment";
 
-    //locators
+    // locators
     private final String nameOnCard = "*[name=name_on_card]";
     private final String cardNumber = "*[name=card_number]";
     private final String cardCvc = "ex. 311";
@@ -32,9 +29,10 @@ public class PaymentPage {
     private final String paymentSuccessMessage = "h2 > b";
     private final String downloadInvoiceButton = "//a[.='Download Invoice']";
 
-    //actions
+    // actions
     @Step("Fill card info")
-    public PaymentPage fillCardInfo(String nameOnCard, String cardNumber, String cardCvc, String cardMonthExpiration, String cardYearExpiration) {
+    public PaymentPage fillCardInfo(String nameOnCard, String cardNumber, String cardCvc, String cardMonthExpiration,
+            String cardYearExpiration) {
         page.locator(this.nameOnCard).fill(nameOnCard);
         page.locator(this.cardNumber).fill(cardNumber);
         page.getByPlaceholder(this.cardCvc).fill(cardCvc);
@@ -56,19 +54,20 @@ public class PaymentPage {
         String userDir = System.getProperty("user.dir");
         String downloadPath = userDir + "\\src\\test\\resources\\downloads";
 
-        download.saveAs(Paths.get(downloadPath ,"invoice.txt"));
+        download.saveAs(Paths.get(downloadPath, "invoice.txt"));
         return this;
     }
 
-    //validations
+    // validations
     @Step("Verify payment success message")
     public PaymentPage verifyPaymentSuccessMessage(String expectedMessage) {
-        Assertions.assertEquals(expectedMessage, page.locator(paymentSuccessMessage).innerText(), "Payment success message is not matched");
+        Assert.assertEquals(page.locator(paymentSuccessMessage).innerText(), expectedMessage,
+                "Payment success message is not matched");
         return this;
     }
 
     public PaymentPage verifyDownloadedFile(String invoiceName) {
-        Assertions.assertTrue(FileUtils.isFileExists(invoiceName), "File is not existed");
+        Assert.assertTrue(FileUtils.isFileExists(invoiceName), "File is not existed");
         return this;
     }
 

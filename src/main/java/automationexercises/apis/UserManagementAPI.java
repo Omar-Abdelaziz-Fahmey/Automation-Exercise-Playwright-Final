4077696f790c53044c955b1dbafeadb43755e73c
Omar <omar.ragh37@gmail.com>
2026-05-25 20:32:15 +0300

    Refactor UserManagementAPI to use Playwright for API requests and improve response handling

--- src/main/java/automationexercises/apis/UserManagementAPI.java
+++ src/main/java/automationexercises/apis/UserManagementAPI.java
@@ -1,115 +1,155 @@
 package automationexercises.apis;
 
+import automationexercises.PlaywrightManager;
 import automationexercises.utils.logs.LogsManager;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.microsoft.playwright.APIRequestContext;
+import com.microsoft.playwright.APIResponse;
+import com.microsoft.playwright.options.FormData;
+import com.microsoft.playwright.options.RequestOptions;
 import io.qameta.allure.Step;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class UserManagementAPI {
-    RequestSpecification requestSpecification;
-    Response response;
+
+    private APIRequestContext apiContext;
+    private APIResponse response;
+    private JsonObject responseJson;
 
     public UserManagementAPI() {
-        // Constructor
-        requestSpecification = RestAssured.given();
+        apiContext= PlaywrightManager.getApiContext();
     }
 
-    // endpoints
-    private static final String createAccount_endpoint = "/createAccount";
-    private static final String deleteAccount_endpoint = "/deleteAccount";
+    private static final String createAccount_endpoint = "/api/createAccount";
+    private static final String deleteAccount_endpoint = "/api/deleteAccount";
 
-    // api methods
+
+    private void parseResponse() {
+        String body = response.text();
+        LogsManager.info(body);
+        responseJson = JsonParser.parseString(body).getAsJsonObject();
+    }
+
+
     @Step("Create a new user account with full details")
-    public UserManagementAPI createRegisterUserAccount(String name, String email, String pass, String title,
-            String birth_date, String birth_month, String birth_year, String firstName, String lastName, String company,
-            String address1, String address2, String country, String zipcode, String state, String city,
+    public UserManagementAPI createRegisterUserAccount(
+            String name, String email, String pass, String title,
+            String birth_date, String birth_month, String birth_year,
+            String firstName, String lastName, String company,
+            String address1, String address2, String country,
+            String zipcode, String state, String city,
             String mobile_number) {
-        Map<String, String> formParams = new HashMap<>();
-        formParams.put("name", name);
-        formParams.put("email", email);
-        formParams.put("password", pass);
-        formParams.put("title", title);
-        formParams.put("birth_date", birth_date);
-        formParams.put("birth_month", birth_month);
-        formParams.put("birth_year", birth_year);
-        formParams.put("firstname", firstName);
-        formParams.put("lastname", lastName);
-        formParams.put("company", company);
-        formParams.put("address1", address1);
-        formParams.put("address2", address2);
-        formParams.put("country", country);
-        formParams.put("zipcode", zipcode);
-        formParams.put("state", state);
-        formParams.put("city", city);
-        formParams.put("mobile_number", mobile_number);
-        response = requestSpecification.spec(Builder.getUserManagementRequestSpecification(formParams))
-                .post(createAccount_endpoint);
-        LogsManager.info(response.asPrettyString());
+
+
+        FormData formData = FormData.create()
+                .set("name", name)
+                .set("email", email)
+                .set("password", pass)
+                .set("title", title)
+                .set("birth_date", birth_date)
+                .set("birth_month", birth_month)
+                .set("birth_year", birth_year)
+                .set("firstname", firstName)
+                .set("lastname", lastName)
+                .set("company", company)
+                .set("address1", address1)
+                .set("address2", address2)
+                .set("country", country)
+                .set("zipcode", zipcode)
+                .set("state", state)
+                .set("city", city)
+                .set("mobile_number", mobile_number);
+
+
+        response = apiContext.post(createAccount_endpoint,
+                RequestOptions.create()
+                        .setForm(formData)
+        );
+
+        parseResponse();
         return this;
     }
 
+
     @Step("Create a new user account with minimal details")
-    public UserManagementAPI createRegisterUserAccount(String name, String email, String pass, String firstName,
-            String lastName) {
-        Map<String, String> formParams = new HashMap<>();
-        formParams.put("name", name);
-        formParams.put("email", email);
-        formParams.put("password", pass);
-        formParams.put("title", "Mr");
-        formParams.put("birth_date", "1");
-        formParams.put("birth_month", "January");
-        formParams.put("birth_year", "1990");
-        formParams.put("firstname", firstName);
-        formParams.put("lastname", lastName);
-        formParams.put("company", "company");
-        formParams.put("address1", "address1");
-        formParams.put("address2", "address2");
-        formParams.put("country", "India");
-        formParams.put("zipcode", "123456");
-        formParams.put("state", "state");
-        formParams.put("city", "city");
-        formParams.put("mobile_number", "1234567890");
-        response = requestSpecification.spec(Builder.getUserManagementRequestSpecification(formParams))
-                .post(createAccount_endpoint);
-        LogsManager.info(response.asPrettyString());
+    public UserManagementAPI createRegisterUserAccount(
+            String name, String email, String pass,
+            String firstName, String lastName) {
+
+        FormData formData = FormData.create()
+                .set("name", name)
+                .set("email", email)
+                .set("password", pass)
+                .set("title", "Mr")
+                .set("birth_date", "1")
+                .set("birth_month", "January")
+                .set("birth_year", "1990")
+                .set("firstname", firstName)
+                .set("lastname", lastName)
+                .set("company", "company")
+                .set("address1", "address1")
+                .set("address2", "address2")
+                .set("country", "India")
+                .set("zipcode", "123456")
+                .set("state", "state")
+                .set("city", "city")
+                .set("mobile_number", "1234567890");
+
+        response = apiContext.post(createAccount_endpoint,
+                RequestOptions.create()
+                        .setForm(formData)
+        );
+
+        parseResponse();
         return this;
     }
 
     @Step("Delete user account by email")
     public UserManagementAPI deleteUserAccount(String email, String password) {
-        Map<String, String> formParams = new HashMap<>();
-        formParams.put("email", email);
-        formParams.put("password", password);
-        response = requestSpecification.spec(Builder.getUserManagementRequestSpecification(formParams))
-                .delete(deleteAccount_endpoint);
-        LogsManager.info(response.asPrettyString());
+
+        FormData formData = FormData.create()
+                .set("email", email)
+                .set("password", password);
+
+        response = apiContext.delete(deleteAccount_endpoint,
+                RequestOptions.create()
+                        .setForm(formData)
+        );
+
+        parseResponse();
         return this;
     }
 
-    // validations
+
     @Step("Verify that user is created successfully")
     public UserManagementAPI verifyUserCreatedSuccessfully() {
-        Assert.assertEquals(response.jsonPath().get("message"), "User created!",
-                "User is not created successfully");
+
+        Assert.assertEquals(
+                responseJson.get("message").getAsString(),
+                "User created!",
+                "User is not created successfully"
+        );
         return this;
     }
 
     @Step("Verify that user is deleted successfully")
     public UserManagementAPI verifyUserDeletedSuccessfully() {
-        Assert.assertEquals(response.jsonPath().get("message"), "Account deleted!",
-                "User is not deleted successfully");
+        Assert.assertEquals(
+                responseJson.get("message").getAsString(),
+                "Account deleted!",
+                "User is not deleted successfully"
+        );
         return this;
     }
 
-    @Step("Verify that user is deleted  while there is the account should not be exist")
+    @Step("Verify that user is deleted while the account should not exist")
     public UserManagementAPI verifyUserDeletedSuccessfullyWhileAccountShouldNotExist() {
-        Assert.assertNotEquals(response.jsonPath().get("message"), "Account deleted!",
-                "User is deleted successfully while there is the account should not be exist");
+        Assert.assertNotEquals(
+                responseJson.get("message").getAsString(),
+                "Account deleted!",
+                "User is deleted successfully while the account should not exist"
+        );
         return this;
     }
 }
